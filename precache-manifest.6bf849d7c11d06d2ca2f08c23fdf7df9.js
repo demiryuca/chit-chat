86c8ab5c4f3a17d4ce11e2e6ce9d95b5faf6e52c
Demir <demiryuca218@gmail.com>
2019-12-29 10:15:49 +0200

    Updates

---
+++ precache-manifest.6bf849d7c11d06d2ca2f08c23fdf7df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3240be685f86bd03839d266a82fd568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8818088c639b87f8396e",
+    "url": "/static/js/2.d0a9942d.chunk.js"
+  },
+  {
+    "revision": "d288d9852b7a728f81251d2a320ba0d0",
+    "url": "/static/js/2.d0a9942d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ae85f0a0ef5a31d37bc",
+    "url": "/static/js/main.b75a5926.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
